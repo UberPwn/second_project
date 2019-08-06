ee987bc138562a4061e472e7b316bd2c52df6076
Davor <zegaracdavor@gmail.com>
2019-08-06 14:44:09 +0200

    Program initialized

---
+++ app.js
@@ -0,0 +1,5 @@
+let math = require('./math.js');
+
+console.log(math.sum([math.pi,10,8]));
+console.log(math.findMax([math.pi,10,8]));
+console.log(math.findMin([math.pi,10,8]));
---
+++ math.js
@@ -0,0 +1,24 @@
+let PI = 3.14;
+let sum = function (elements) {
+    let total = 0;
+    for (i=0;i<elements.length;i++) total+=elements[i];
+    return total;
+}
+
+let findMax = function (elements) {
+    let max = elements[0];
+    for (i=1;i<elements.length;i++) if (max<elements[i]) max = elements[i];
+    return max;
+}
+
+
+let findMin = function (elements) {
+    let min = elements[0];
+    for (i=1;i<elements.length;i++) if (min>elements[i]) min = elements[i];
+    return min;
+}
+
+module.exports.pi = PI;
+module.exports.sum = sum;
+module.exports.findMax = findMax;
+module.exports.findMin = findMin;
