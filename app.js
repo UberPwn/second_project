e881a6c3108d249ecdfa9d3226300098519e189b
Davor <zegaracdavor@gmail.com>
2019-08-06 14:47:51 +0200

    testing changes

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ let math = require('./math.js');
 
 console.log(math.sum([math.pi,10,8]));
 console.log(math.findMax([math.pi,10,8]));
-console.log(math.findMin([math.pi,10,8]));
+console.log(math.findMin([math.pi,10,7]));
--- math.js
+++ math.js
@@ -11,13 +11,11 @@ let findMax = function (elements) {
     return max;
 }
 
-
 let findMin = function (elements) {
     let min = elements[0];
     for (i=1;i<elements.length;i++) if (min>elements[i]) min = elements[i];
     return min;
 }
-
 module.exports.pi = PI;
 module.exports.sum = sum;
 module.exports.findMax = findMax;
